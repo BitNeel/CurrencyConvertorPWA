b853cd7dcb6c30f7b1103ff4fa7d7e85bb4cd306
Aditya Poddar <aditya@spikewell.com>
2020-09-13 10:29:58 +0530

    custom css for active link animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from "react";
 import {getCountries} from './api';
 import AppHeader from './components/AppHeader'
 import ConvertorForm from './components/ConvertorForm';
+import './App.css';
 function App() {
   const [countryList, setcountryList] = useState({})
   useEffect(
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import templates from '../template.json';
 export default function AppHeader() {
+    const addEnterClass=e=>{
+        let targetNode = document.getElementById(e.target.id);
+        targetNode.classList.add("navHover")
+    }
+    const removeEnterClass=e=>{
+        let targetNode = document.getElementById(e.target.id);
+        targetNode.classList.remove("navHover")
+    }
+    const addActiveClass=e=>{
+        let targetNodes = document.getElementsByClassName("navItemSpan");
+        for(let i=0;i<targetNodes.length;i++)
+        {
+            targetNodes[i].classList.remove("navActive");
+        }
+        let currentnode = document.getElementById(e.target.id);
+        currentnode.classList.add("navActive");
+    }
     return (
-    <nav className="navbar navbar-light bg-light">
-        <span className="display-4 mx-auto">{templates.AppHeader}</span>
-    </nav>
-    )
+      <React.Fragment>
+        <nav className="navbar navbar-light bg-light">
+          <span className="display-4 mx-auto">{templates.AppHeader}</span>
+        </nav>
+        <ul className="nav nav-pills nav-fill bg-light" id="navList">
+          {templates.NavLinks.map((link, index) => (
+            <li key={index} className="nav-item">
+              <span
+                id={`nav-item-span${index}`}
+                className="navItemSpan"
+                onMouseEnter={addEnterClass}
+                onMouseLeave={removeEnterClass}
+                onClickCapture={addActiveClass}
+              >
+                {link}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
 }
