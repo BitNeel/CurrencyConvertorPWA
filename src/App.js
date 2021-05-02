ccf043be3bb0455a93f85d7750b8921dfae885cf
Aditya Poddar <poddarneel@gmail.com>
2021-05-02 22:22:10 +0530

    changed API key

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React,{useEffect,useState} from "react";
-import {getCurrencies} from './api';
-import AppHeader from './components/AppHeader'
-import ConvertorForm from './components/ConvertorForm';
-import CurrencyList from './components/CurrencyList';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getCurrencies } from "./api";
+import AppHeader from "./components/AppHeader";
+import ConvertorForm from "./components/ConvertorForm";
+import CurrencyList from "./components/CurrencyList";
+import "./App.css";
 function App() {
-  const [countryList, setcountryList] = useState({})
-  const [toggle,setToggle] = useState(0);
-  useEffect(
-    ()=>{
-      const catchCountries = async ()=>{ 
-        let {results} = await getCurrencies();
-        setcountryList(results);
-      };
+  const [countryList, setcountryList] = useState({});
+  const [toggle, setToggle] = useState(0);
+  const [apierror, setAPIError] = useState("");
+  useEffect(() => {
+    const catchCountries = async () => {
+      let { results } = await getCurrencies();
+      setcountryList(results);
+    };
+    try {
       catchCountries();
+    } catch (exception) {
+      setAPIError("Something Went wrong");
     }
-  ,
-  []
-  )
+  }, []);
   return (
     <React.Fragment>
-    <AppHeader setToggle={setToggle}/>
-    {
-      !toggle?<ConvertorForm countryList={countryList}/>:
-      <CurrencyList/>
-    }
+      <AppHeader setToggle={setToggle} />
+      {apierror ? (
+        <span>{apierror}</span>
+      ) : (
+        <>
+          {!toggle ? (
+            <ConvertorForm countryList={countryList} />
+          ) : (
+            <CurrencyList />
+          )}
+        </>
+      )}
     </React.Fragment>
   );
 }
